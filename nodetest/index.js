a64312fa3949064b7e7b7d1e1fa2865881f1b814
waseemakhter <waseemakhter028@gmail.com>
2024-05-06 23:46:34 +0530

    node test project

--- nodetest/index.js
+++ nodetest/index.js
@@ -1,20 +1,93 @@
-// index.js
-const express = require('express')
+import 'dotenv/config'
+import express from 'express';
+import bodyParser from 'body-parser'
+import path from 'path'
+import helmet from 'helmet';
+import compression from 'compression'
+import cors from 'cors'
+import {fileURLToPath} from 'url';
+import fileUpload from 'express-fileupload'
+// import swaggerUi from 'swagger-ui-express'
+// import swaggerDocument from './swagger.json' assert{ type: "json" }
+// import * as errorHandler from './middlewares/error-handler.js'
+// import Routes from './routes/index.js';
+// import connectDB from './config/db.js';
 
 const app = express()
-const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+global.appRoot = path.join(__dirname)
+
+// connectDB()
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false
+  })
+)
+app.use(
+  compression({
+    level: 6,
+    threshold: 10 * 100,
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        return false
+      }
+      return compression.filter(req, res)
+    }
+  })
+)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// fileupload middleware use
+app.use(fileUpload())
+
+app.use(cors())
+app.options('*', cors())
+// Swagger Docs
+// app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.get('/', async (req, res) => {
+  res.send('working server test')
 })
 
-app.get('/', (req, res) => {
-  res.send('working server')
+// Api Routes
+// app.use('/', Routes)
+
+// Error Middlewares
+//404 not found handle
+// app.use(errorHandler.notFound)
+// generic Error handler
+// app.use(errorHandler.genericErrorHandler)
+
+//server configuration
+const server = app.listen(process.env.PORT, async () => {
+  console.log(`Server up successfully - host: ${process.env.HOST} port: ${process.env.PORT}`)
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+process.on('unhandledRejection', (err) => {
+  console.log('possibly unhandled rejection happened')
+  console.log(err.message)
 })
 
-// Export the Express API
-module.exports = app
+const closeHandler = () => {
+  // eslint-disable-next-line no-undef
+  Object.values(connections).forEach((connection) => connection.close())
+
+  server.close(() => {
+    console.log('Server is stopped succesfully')
+    process.exit(0) /* eslint-disable-line */
+  })
+}
+
+process.on('SIGTERM', closeHandler)
+process.on('SIGINT', closeHandler)
+
+export default app
