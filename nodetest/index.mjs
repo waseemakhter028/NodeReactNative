232a02ed19c9a123e08440c5016d73869bb81589
waseemakhter <waseemakhter028@gmail.com>
2024-05-07 13:39:34 +0530

    node test fixed

--- nodetest/index.mjs
+++ nodetest/index.mjs
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import express from 'express';
-// import bodyParser from 'body-parser'
-// import path from 'path'
-// import helmet from 'helmet';
-// import compression from 'compression'
-// import cors from 'cors'
-// import {fileURLToPath} from 'url';
-// import fileUpload from 'express-fileupload'
+import bodyParser from 'body-parser'
+import path from 'path'
+import helmet from 'helmet';
+import compression from 'compression'
+import cors from 'cors'
+import {fileURLToPath} from 'url';
+import fileUpload from 'express-fileupload'
 // import swaggerUi from 'swagger-ui-express'
 // import swaggerDocument from './swagger.json' assert{ type: "json" }
 // import * as errorHandler from './middlewares/error-handler.js'
@@ -15,42 +15,42 @@ import express from 'express';
 
 const app = express()
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// global.appRoot = path.join(__dirname)
+global.appRoot = path.join(__dirname)
 
 // connectDB()
 
-// app.use(
-//   helmet({
-//     crossOriginResourcePolicy: false
-//   })
-// )
-// app.use(
-//   compression({
-//     level: 6,
-//     threshold: 10 * 100,
-//     filter: (req, res) => {
-//       if (req.headers['x-no-compression']) {
-//         return false
-//       }
-//       return compression.filter(req, res)
-//     }
-//   })
-// )
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'ejs')
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false
+  })
+)
+app.use(
+  compression({
+    level: 6,
+    threshold: 10 * 100,
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        return false
+      }
+      return compression.filter(req, res)
+    }
+  })
+)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use(bodyParser.json({ limit: '2mb' }))
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// // fileupload middleware use
-// app.use(fileUpload())
+// fileupload middleware use
+app.use(fileUpload())
 
-// app.use(cors())
-// app.options('*', cors())
+app.use(cors())
+app.options('*', cors())
 // Swagger Docs
 // app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
