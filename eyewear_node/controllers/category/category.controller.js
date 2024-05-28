444f0e9b5fa34ad148ff2e957e87f0b6d6d356b2
waseemakhter <waseemakhter028@gmail.com>
2024-05-28 15:02:59 +0530

    category sorting added

--- eyewear_node/controllers/category/category.controller.js
+++ eyewear_node/controllers/category/category.controller.js
@@ -1,63 +1,62 @@
 /* global LOCALE */
-const Product    = require('../../models/product')
-const Category   = require('../../models/category')
-
+const Product = require("../../models/product");
+const Category = require("../../models/category");
 
 const getAllCategories = async (req, res) => {
   try {
-    
-      //  let  cateIds = await Product.distinct('category') laravel pluck example
-      
-      //  //get all categories if has atleast one product
-      //  const rows = await Category.find({_id: {$in: cateIds} }).sort({createdAt:-1}) 
+    //  let  cateIds = await Product.distinct('category') laravel pluck example
 
-       const rows = await Product.aggregate([       
-        {
-          $lookup:{
-             from:'categories',
-             localField:'category',
-             foreignField:'_id',
-             as:'cat'
-          }
+    //  //get all categories if has atleast one product
+    //  const rows = await Category.find({_id: {$in: cateIds} }).sort({createdAt:-1})
+
+    const rows = await Product.aggregate([
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "cat",
         },
-        //use unwind to array to object conversion becasue mongodb alwayas return array
-        {
-           $unwind:"$cat"
-        }, 
+      },
+      //use unwind to array to object conversion becasue mongodb alwayas return array
+      {
+        $unwind: "$cat",
+      },
 
-        {
-         $project: {
-           _id:0,
-           "categoryId":"$cat._id",
-           "name":"$cat.name"
-         }
+      {
+        $project: {
+          _id: 0,
+          categoryId: "$cat._id",
+          name: "$cat.name",
         },
+      },
 
-        {
-         $group : {
-          _id:"$categoryId",
-          categoryId: { $first: '$categoryId' },
-          name: { $first: '$name' }
-        }
-       },
-       
-       {
-         $project: {
-          _id:0, 
-          name:1,
-          id:"$categoryId"
-         }
-       }
-       
-     ]);
+      {
+        $group: {
+          _id: "$categoryId",
+          categoryId: { $first: "$categoryId" },
+          name: { $first: "$name" },
+        },
+      },
 
-       return helper.sendSuccess(rows, res, req.t("data_retrived"),200)
-    
+      {
+        $project: {
+          _id: 0,
+          name: 1,
+          id: "$categoryId",
+        },
+      },
+      {
+        $sort: { name: 1 },
+      },
+    ]);
+
+    return helper.sendSuccess(rows, res, req.t("data_retrived"), 200);
   } catch (e) {
-    return helper.sendException(res, e.message, e.code)
+    return helper.sendException(res, e.message, e.code);
   }
 };
 
 module.exports = {
-  getAllCategories
+  getAllCategories,
 };
