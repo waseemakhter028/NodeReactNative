d5f828c383b8ea4fbcefda2e4f6ec8fdc8879122
waseemakhter <waseemakhter028@gmail.com>
2024-05-31 23:16:03 +0530

    node js server close handle

--- eyewear_node/server.js
+++ eyewear_node/server.js
@@ -102,26 +102,26 @@ app.use(errorHandler.notFound);
 // module.exports = server;
 
 //server configuration
-const server = app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT, async () => {
   console.log(
     `Server up successfully - host: ${process.env.HOST} port: ${process.env.PORT}`
   );
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log("possibly unhandled rejection happened");
-  console.log(err.message);
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("possibly unhandled rejection happened");
+//   console.log(err.message);
+// });
 
-const closeHandler = () => {
-  // eslint-disable-next-line no-undef
-  Object.values(connections).forEach((connection) => connection.close());
+// const closeHandler = () => {
+//   // eslint-disable-next-line no-undef
+//   Object.values(connections).forEach((connection) => connection.close());
 
-  server.close(() => {
-    console.log("Server is stopped succesfully");
-    process.exit(0); /* eslint-disable-line */
-  });
-};
+//   server.close(() => {
+//     console.log("Server is stopped succesfully");
+//     process.exit(0); /* eslint-disable-line */
+//   });
+// };
 
-process.on("SIGTERM", closeHandler);
-process.on("SIGINT", closeHandler);
+// process.on("SIGTERM", closeHandler);
+// process.on("SIGINT", closeHandler);
